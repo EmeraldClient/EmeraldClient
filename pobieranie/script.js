6b8631e6da68e267309a5d641724efd1892d4fc1
MarchewaNowaksk <>
2021-09-11 13:00:52 +0000

    Initial commit

---
+++ pobieranie/script.js
@@ -0,0 +1,12 @@
+function onHome() {
+  window.location.href = 'https://EmeraldClient.marchewanowaksk.repl.co';
+}
+
+function Kontakt() {
+  window.location.href = 'https://EmeraldClient.marchewanowaksk.repl.co/Kontakt';
+}
+
+function Blacklista() {
+  window.location.href = 'https://EmeraldClient.marchewanowaksk.repl.co/Blacklista';
+}
+
---
+++ script.js
@@ -0,0 +1,11 @@
+function clickDC() {
+  window.location.href = 'https://discord.gg/FnK28W2ZcG';
+}
+
+function clickDownload() {
+  window.location.href = 'index.php';
+}
+
+function Pobieranie() {
+  window.location.href = 'https://EmeraldClient.marchewanowaksk.repl.co/pobieranie';
+}
